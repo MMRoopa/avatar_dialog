a9393afaeaab7f52dcd5117fdd79fc15bdea9001
MMRoopa <mailtoroopa@gmail.com>
2022-01-31 12:31:40 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  final int currentIndex = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AvatarDialog(currentIndex: currentIndex),
+              );
+            },
+            child: const Text('Avatar dialog'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AvatarDialog extends StatelessWidget {
+  AvatarDialog({Key? key, required this.currentIndex}) : super(key: key);
+
+  final int currentIndex;
+  final List<Map> myIcons =
+      List.generate(30, (index) => {"id": index, "name": "Product $index"})
+          .toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: const Color(0xFFFFFFFF),
+      alignment: Alignment.bottomCenter,
+      insetPadding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(35.0)),
+      ),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.7,
+        width: MediaQuery.of(context).size.width,
+        child: Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 44.0),
+                child: AutoSizeText(
+                  "Select Avatar",
+                  style: TextStyle(
+                    color: Color(0xFF1353CB),
+                    fontSize: 26.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                  ),
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.fromLTRB(15.0, 22.0, 15.0, 25.0),
+                child: GridView.builder(
+                    physics: const ScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 3,
+                      crossAxisSpacing: 3,
+                    ),
+                    itemCount: myIcons.length,
+                    // margin: const EdgeInsets.fromLTRB(15.0, 22.0, 15.0, 25.0),
+                    itemBuilder: (context, index) {
+                      return ClipOval(
+                        child: InkWell(
+                          //overlayColor: Colors.amber,
+                          // highlightColor: Colors.amber,
+                          splashColor: Colors.blue.withAlpha(30),
+                          onTap: () {
+                            // print('Tapped on thumbnail.');
+                          },
+                          child: Container(
+                              margin:
+                                  const EdgeInsets.only(top: 5.0, right: 15.0),
+                              height: 85.0,
+                              width: 80.0,
+                              alignment: Alignment.center,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.lime),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    ClipRect(
+                                      child: Image.asset(
+                                        'assets/images/group_30368.png',
+                                        width: 81.0,
+                                        height: 76.0,
+                                        fit: BoxFit.fill,
+                                        alignment: Alignment.center,
+                                      ),
+                                    ),
+                                  ])),
+                        ),
+                      );
+                    }),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 25.0, bottom: 21.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 50.0,
+                    width: 243.0,
+                    child: const AutoSizeText(
+                      'CONTINUE',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                      maxLines: 1,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF1353CB),
+                      borderRadius: BorderRadius.all(Radius.circular(60.0)),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
