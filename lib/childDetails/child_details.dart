f812a489edc9f2cbe76bcb85ca1af64895c3bb51
MMRoopa <mailtoroopa@gmail.com>
2022-02-17 11:46:22 +0530

    Expanded removed for ListView

--- lib/childDetails/child_details.dart
+++ lib/childDetails/child_details.dart
@@ -35,8 +35,8 @@ class _ChildDetailsState extends State<ChildDetails> {
             childGrade(context),
             childCity(),
             childSchool(),
-            childLanguage(context),
-            // selectWhatsapp(),
+            // childLanguage(context),
+            selectWhatsapp(),
             startTrial(),
           ],
         ),
@@ -67,7 +67,7 @@ class _ChildDetailsState extends State<ChildDetails> {
     return Container(
       height: 60.h,
       width: 338.w,
-      margin: EdgeInsets.only(left: 19.w, top: 20.h, bottom: 64.h, right: 18.w),
+      margin: EdgeInsets.only(left: 19.w, bottom: 64.h, right: 18.w),
       decoration: const BoxDecoration(
         color: Color(0xFF1353CB),
         borderRadius: BorderRadius.all(Radius.circular(50.0)),
@@ -102,7 +102,7 @@ class _ChildDetailsState extends State<ChildDetails> {
     return Container(
       height: 23.h,
       width: 256.w,
-      margin: EdgeInsets.only(bottom: 37.h),
+      margin: EdgeInsets.only(top: 20.h, bottom: 37.h),
       child: GestureDetector(
         onTap: () {
           setState(() {
@@ -154,7 +154,7 @@ class _ChildDetailsState extends State<ChildDetails> {
 
   Container childLanguage(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(left: 19.w, right: 18.w, bottom: 20.h),
+      margin: EdgeInsets.only(left: 19.w, right: 18.w, bottom: 40.h),
       child: TextField(
         readOnly: true,
         controller: langController,
