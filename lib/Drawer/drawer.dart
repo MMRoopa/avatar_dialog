5af30436459840528a98dc9d9a583f6f64165edb
MMRoopa <mailtoroopa@gmail.com>
2022-03-22 19:12:24 +0530

    gap between leading icon and the text is fixed, padding added for all items in the list tile

--- lib/Drawer/drawer.dart
+++ lib/Drawer/drawer.dart
@@ -87,41 +87,46 @@ class _DrawerWidgetState extends State<DrawerWidget> {
 
   Container listItems() {
     return Container(
-      height: 390.h,
-      width: 294.w,
+      height: 543.h,
+      width: 293.w,
       margin: EdgeInsets.only(left: 10.w, right: 10.w),
       alignment: AlignmentDirectional.centerStart,
       child: ListView.builder(
-          shrinkWrap: true,
           itemCount: 5, //items.length,
           itemBuilder: (BuildContext context, index) {
             return ListTile(
-              contentPadding:
-                  EdgeInsets.only(right: 10.w, top: 10.h, bottom: 10.h),
+              contentPadding: EdgeInsets.only(
+                  left: 14.w, top: 5.h, right: 10.w, bottom: 5.h),
+              minVerticalPadding: 10.0,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0)),
               selectedTileColor: const Color(0xFFEFF0FF),
               trailing: Padding(
-                padding: EdgeInsets.only(right: 5.w),
+                padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
                 child: const Icon(
                   Icons.keyboard_arrow_right_outlined,
                   color: Colors.black,
                   size: 20,
                 ),
               ),
-              title: Text(
-                items[index]['title'],
-                style: TextStyle(
-                  fontSize: 14.sp,
-                  color: const Color(0xFF2B2B2B),
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w500,
+              horizontalTitleGap: 10.w,
+              title: Padding(
+                padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
+                child: Text(
+                  items[index]['title'],
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    color: const Color(0xFF2B2B2B),
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 1,
+                  textAlign: TextAlign.left,
                 ),
-                maxLines: 1,
-                textAlign: TextAlign.left,
               ),
+              minLeadingWidth: 24.w,
               leading: Padding(
-                padding: const EdgeInsets.only(left: 15.0),
+                padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
                 child: Image.asset(
                   items[index]["icon"],
                   fit: BoxFit.scaleDown,
@@ -137,11 +142,10 @@ class _DrawerWidgetState extends State<DrawerWidget> {
     );
   }
 
-  Container footerImage() {
-    return Container(
+  SizedBox footerImage() {
+    return SizedBox(
       width: 314.w,
       height: 114.h,
-      margin: EdgeInsets.only(top: 153.h),
       child: ClipRRect(
         borderRadius: const BorderRadius.only(bottomRight: Radius.circular(20)),
         child: Image.asset(
@@ -182,20 +186,17 @@ class _DrawerWidgetState extends State<DrawerWidget> {
               Container(
                 height: 75.h,
                 width: 75.w,
+                margin: EdgeInsets.fromLTRB(10.w, 11.h, 8.w, 11.h),
                 decoration: const BoxDecoration(shape: BoxShape.circle),
                 child: Image.asset(
                   'assets/images/group_30368.png',
                   height: 61.h,
                   width: 57.w,
-                  //fit: BoxFit.cover,
                   alignment: Alignment.center,
                 ),
               ),
               SizedBox(
-                width: 8.w,
-              ),
-              SizedBox(
-                width: 159.w,
+                width: 165.w,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -230,11 +231,9 @@ class _DrawerWidgetState extends State<DrawerWidget> {
                 ),
               ),
               Container(
-                alignment: Alignment.centerRight,
-                margin: EdgeInsets.only(left: 10.w, right: 10.w),
+                margin: EdgeInsets.only(right: 10.w),
                 child: const Icon(
                   Icons.keyboard_arrow_right_outlined,
-                  // Icons.arrow_forward_ios_sharp,
                   color: Colors.white,
                   size: 20,
                 ),
